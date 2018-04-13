9edde0647c21c637cde3d59af5dd0d95567ce5f4
Vincent Lachenal <vincent.lachenal@gmail.com>
2018-04-13 21:35:18 +0200

    Move from component to repository stereotype for statistics DAO

--- src/main/java/com/github/vlachenal/webservice/bench/dao/StatisticsDAO.java
+++ src/main/java/com/github/vlachenal/webservice/bench/dao/StatisticsDAO.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.vlachenal.webservice.bench.dto.TestSuiteDTO;
@@ -25,7 +25,7 @@ import com.github.vlachenal.webservice.bench.dto.TestSuiteDTO;
  *
  * @author Vincent Lachenal
  */
-@Component
+@Repository
 public class StatisticsDAO {
 
   // Attributes +
