89a5e0163add55cc583da77cdc5bb4ecf7455a3b
Vincent Lachenal <vincent.lachenal@gmail.com>
2018-06-02 19:16:47 +0200

    plop

--- src/main/java/com/github/vlachenal/webservice/bench/YamlSingleDocPropertySourceFactory.java
+++ src/main/java/com/github/vlachenal/webservice/bench/YamlSingleDocPropertySourceFactory.java
@@ -29,8 +29,8 @@ public class YamlSingleDocPropertySourceFactory implements PropertySourceFactory
    */
   @Override
   public PropertySource<?> createPropertySource(final String name, final EncodedResource resource) throws IOException {
-    final YamlPropertySourceLoader sourceLoader = new YamlPropertySourceLoader();
-    final List<PropertySource<?>> sources = sourceLoader.load(name, resource.getResource());
+    final YamlPropertySourceLoader srcLoader = new YamlPropertySourceLoader();
+    final List<PropertySource<?>> sources = srcLoader.load(name, resource.getResource());
     return sources == null ? null : sources.get(0);
   }
 
